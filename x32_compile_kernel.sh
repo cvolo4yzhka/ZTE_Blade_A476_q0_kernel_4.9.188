e38dd9238cb39611877dc69958633dbd8191059e
cvolo4yzhka <cvolo4yzhka@mail.ru>
2020-03-04 22:06:07 +0300

    any

--- x32_compile_kernel.sh
+++ x32_compile_kernel.sh
@@ -2,7 +2,7 @@
 #set architectute
 export ARCH=arm
 #set cross compiller path /toolchain
-export CROSS_COMPILE=~/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/source/zte_a476/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 #set directory for build output
 export  KBUILD_OUTPUT=out32
 #set defconfig
