650e27a637c44d87a20124687660bb87866d79a1
cvolo4yzhka <cvolo4yzhka@mail.ru>
2020-11-06 21:01:55 +0300

    patch to 4.9.204

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2795,6 +2795,8 @@ int f2fs_release_page(struct page *page, gfp_t wait)
 	if (PageDirty(page))
 		return 0;
 
+	clear_cold_data(page);
+
 	/* This is atomic written page, keep Private */
 	if (IS_ATOMIC_WRITTEN_PAGE(page))
 		return 0;
