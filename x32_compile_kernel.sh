beac9bc36f32e690f398640dd9c78de780480379
cvolo4yzhka <cvolo4yzhka@mail.ru>
2020-11-06 13:20:53 +0300

    ***

--- x32_compile_kernel.sh
+++ x32_compile_kernel.sh
@@ -2,11 +2,11 @@
 #set architectute
 export ARCH=arm
 #set cross compiller path /toolchain
-export CROSS_COMPILE=~/source/zte_a476/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/alps/q0_alps_zte_a476/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 #set directory for build output
 export  KBUILD_OUTPUT=out32
 #set defconfig
 make zte_blade_a476_defconfig
 #start compile
-make zImage-dtb -j1 2>&1 | tee out32/build.log
+make zImage-dtb -j16 2>&1 | tee out32/build.log
 #make menuconfig
